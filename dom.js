a07e49088bc70bae4877e1a208fb9d9bbdfd4ad3
monoronjon sarker <monoronjons86@gmail.com>
2023-05-29 20:25:43 +0600

    mmm

---
+++ dom.js
@@ -0,0 +1,21 @@
+document.getElementById('apply-bg').addEventListener('click',function(){
+    const friends=document.getElementsByClassName('friend');
+    for(const friend of friends){
+        friend.style.backgroundColor='red';
+    }
+});
+
+document.getElementById('center-third').addEventListener('click',function(){
+    const third=document.getElementById('third-friend');
+    third.style.textAlign='center';
+})
+document.getElementById('add-friend').addEventListener('click',function(){
+    const friendContainer=document.getElementById('friends');
+    const friend=document.createElement('div');
+    friend.innerHTML=`
+    <h3 class='friend-name'>New Friend</h3>
+    <p>something new added</p>
+    `
+    friendContainer.appendChild(friend);
+
+});
