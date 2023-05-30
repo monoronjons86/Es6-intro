fc81a4b558b1e5f31563613783377b08bbbb9765
monoronjon sarker <monoronjons86@gmail.com>
2023-05-30 12:05:17 +0600

    templete string end

---
+++ template.js
@@ -0,0 +1,25 @@
+const person='Adam sandler';
+const person2="Ben white";
+const person3=`Donal Trump`;
+
+const multiline='first line text \n '+
+'second line text \n'+
+'third line text \n'+
+'fourth line text \n';
+// console.log(multiline);
+
+
+const newMultiLine=`First line of text
+second line of text
+third line of text
+fourth line of text`
+// console.log(newMultiLine);
+
+const a=20;
+const b=30;
+const nums=[78,98,45,12,6,444]
+const summary='sum of: ' + a + ' and ' + b + ' is '+(a+b); 
+// console.log(summary);
+const newSummary=`sum of ${a} and ${nums.length} is :${a+b}`;
+console.log(newSummary);
+
