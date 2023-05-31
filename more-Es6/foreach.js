357737d4025856b6704f71d939027daa9c838667
monoronjon sarker <monoronjons86@gmail.com>
2023-06-01 00:55:17 +0600

    night finished

---
+++ more-Es6/foreach.js
@@ -0,0 +1,8 @@
+const friends=['Tom Hanks','Tom Cruise','Tom Brady','Tom Solaiman'];
+friends.forEach(friend =>console.log(friend))
+const products=[
+    {id:1,name:'laptop',price:4500},
+    {id:1,name:'Mobile',price:8000},
+    {id:1,name:'Watch',price:35000},
+    {id:1,name:'Tablet',price:23000}
+];
