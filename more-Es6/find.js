9361a0f8eb3e22f5b476a684095466955752cafc
monoronjon sarker <monoronjons86@gmail.com>
2023-06-01 16:29:07 +0600

    notain add

---
+++ more-Es6/find.js
@@ -0,0 +1,10 @@
+const numbers=[12,13.14,30,31,65,70,78];
+const fives=numbers.find(num=>num%5===0);
+console.log(fives);
+
+const products=[
+    {id:1,name:'laptop',price:4500},
+    {id:1,name:'Mobile',price:8000},
+    {id:1,name:'Watch',price:35000},
+    {id:1,name:'Tablet',price:23000}
+];
---
+++ more-Es6/notain.js
@@ -0,0 +1,17 @@
+const student={
+    name:'monoronjon sarker',
+    age:15,
+    class:'Ten',
+    marks:{
+        math:78,
+        physics:89,
+        chemistry:65
+    }
+}
+const marks=student.marks;
+const math=student.marks.math;
+const chemistry=student['marks']['chemistry']
+// console.log(chemistry)
+const subject='math';
+const subjectMarks=student.marks[subject];
+console.log(subjectMarks);
