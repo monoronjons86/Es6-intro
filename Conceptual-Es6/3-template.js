8f406ac82adb2c60f26d1f9e085e6a271d3da79f
monoronjon sarker <monoronjons86@gmail.com>
2023-06-04 17:37:38 +0600

    template string end

---
+++ Conceptual-Es6/3-template.js
@@ -0,0 +1,11 @@
+const Name='monoronjon sarker';
+const age=43;
+const job='bakar';
+
+// const sentence='amar name ' + Name+ '\n boyos ' + age +'\n bose khai '+job;
+
+const sentence=`amar name ${Name}
+amar boys ${age}
+ami ${job}
+`
+console.log(sentence);
