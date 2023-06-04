2e28df94f07c676aad55d92afad2fb4ec55d27a4
monoronjon sarker <monoronjons86@gmail.com>
2023-06-04 19:56:24 +0600

    ternary-end

---
+++ Conceptual-Es6/4-ternery.js
@@ -0,0 +1,9 @@
+const isStudent=true;
+let paragraph='';
+// if(isStudent===true){
+//     paragraph='he is student';
+// }
+// else{
+//     paragraph='pora suna vlo lage na';
+// }
+(isStudent===true) ? console.log('he is student'):console.log('not a student');
