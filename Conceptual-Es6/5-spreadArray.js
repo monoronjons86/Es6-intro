7a9a4d901cf85dfc98a797fd0e8abb5fd66abc8b
monoronjon sarker <monoronjons86@gmail.com>
2023-06-05 13:56:01 +0600

    spreadEnd

--- Conceptual-Es6/5-spreadArray.js
+++ Conceptual-Es6/5-spreadArray.js
@@ -6,4 +6,19 @@ console.log(Math.max(...number));
 const newArr=[123,21,24,32,...number2,99];
 console.log(newArr);
 
-const arr1=[1,2,3,4,5]
+const arr1=[1,2,3,4,5];
+const arr2=[...arr1];
+
+arr1.push(99);
+arr2.push(55);
+// console.log(arr1);
+// console.log(arr2);
+
+const arr=[1,2,3,4,5,6,7];
+// const first=arr[0];
+// const second=arr[1];
+// console.log(first,second);
+
+const [first,second,...remaining]=arr;
+console.log(first,second);
+console.log(remaining);
