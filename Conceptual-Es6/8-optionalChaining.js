f0d1f48d147af74169395c8cb29190e7a5c9279c
monoronjon sarker <monoronjons86@gmail.com>
2023-06-05 23:01:09 +0600

    optinal-chaining

---
+++ Conceptual-Es6/8-optionalChaining.js
@@ -0,0 +1,17 @@
+const petOwner={
+    name:'Hero alom',
+    contact:{
+        phone:'01723927596',
+        email:'xyz@gmail.com'
+    },
+    pet:{
+        name:'Lofie',
+        info:{
+            color:'black',
+            weight:'1.2kg',
+
+        },
+    },
+}
+// console.log(petOwner.dog.name)
+console.log(petOwner?.dog?.name)
