52dcf3364abd4f0664078686ab937337eeb212a5
monoronjon sarker <monoronjons86@gmail.com>
2023-06-07 09:40:48 +0600

    map practice-end

--- more-Es6/map.js
+++ more-Es6/map.js
@@ -1,4 +1,4 @@
-const numbers=[2,8,4,6,3];
+// const numbers=[2,8,4,6,3];
 // const output=[];
 // for(const number of numbers){
 //     const doubled=number*2;
@@ -26,8 +26,8 @@ const numbers=[2,8,4,6,3];
 // const makeDouble=numbers.map(doubleIt);
 // console.log(makeDouble);
 
-const fiveTimes=[1,2,3,4,5].map(x=>x*5);
-console.log(fiveTimes);
+// const fiveTimes=[1,2,3,4,5].map(x=>x*5);
+// console.log(fiveTimes);
 
 /*
 purpose:
@@ -36,3 +36,36 @@ purpose:
 3.store the result in an array
 4.return the result array
 */
+
+//==================again see and practice here======================
+const numbers=[2,8,6,4,10]
+const output=[];
+
+console.log(output)
+
+function getDoubles(){
+    const output=[];
+    for(const number of numbers){
+        const doubled=doubleIt(number)
+        output.push(doubled)
+    }
+    return output;
+}
+// function doubleIt(number){
+//     return number*2;
+
+// }
+const doubleIt=nam=>nam*2;
+const result=getDoubles(numbers);
+console.log(result);
+const makeDouble=numbers.map(doubleIt);
+console.log(makeDouble)
+/*
+purpose:
+1.get and array
+2.for every elements of the array do something 
+3.store the result in an array
+4.return the result array
+
+*/
+
--- more-Es6/more-map.js
+++ more-Es6/more-map.js
@@ -1,21 +1,44 @@
+// const numbers=[12,56,87,44];
+// const half=numbers.map(n=>n/2);
+// const thirds=numbers.map(x=>x/3);
+// // console.log(half);
+// // console.log(thirds);
+// const friends=['Tom Hanks','Tom Cruise','Tom Brady','Tom Solaiman'];
+// const firstLetters=friends.map(friend=>friend[0]);
+// // console.log(firstLetters);
+// const nameLengths=friends.map(friend=>friend.length);
+// // console.log(nameLengths);
+// const products=[
+//     {id:1,name:'laptop',price:4500},
+//     {id:1,name:'Mobile',price:8000},
+//     {id:1,name:'Watch',price:35000},
+//     {id:1,name:'Tablet',price:23000}
+// ];
+// // const items=products.map(product =>console.log(product.name));
+// const items=products.map(product=>product.name);
+// // console.log(items);
+// const prices=products.map(p=>p.price);
+// console.log(prices);
+
+//================== again try lan map kore de vi====================
 const numbers=[12,56,87,44];
 const half=numbers.map(n=>n/2);
 const thirds=numbers.map(x=>x/3);
-// console.log(half);
-// console.log(thirds);
-const friends=['Tom Hanks','Tom Cruise','Tom Brady','Tom Solaiman'];
-const firstLetters=friends.map(friend=>friend[0]);
+console.log(half);
+const friends=['Tom Hanks','Tom Cruise','Tom Brady','Tom solaiman'];
+const firstLetters=friends.map(friend=>friend);
 // console.log(firstLetters);
 const nameLengths=friends.map(friend=>friend.length);
-// console.log(nameLengths);
+console.log(nameLengths);
+
 const products=[
     {id:1,name:'laptop',price:4500},
-    {id:1,name:'Mobile',price:8000},
-    {id:1,name:'Watch',price:35000},
-    {id:1,name:'Tablet',price:23000}
+    {id:2,name:'Iphone',price:8500},
+    {id:3,name:'watch',price:4000},
+    {id:1,name:'tab',price:25000}
 ];
-// const items=products.map(product =>console.log(product.name));
+// const items=products.map(product=>console.log(product))
 const items=products.map(product=>product.name);
-// console.log(items);
+console.log(items);
 const prices=products.map(p=>p.price);
 console.log(prices);
